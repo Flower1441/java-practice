c8321d5159d8532301c6f5e857eb571a54bbc1c5
小花 <xiaohua@example.com>
2026-06-30 17:05:34 +0800

    科学计算器: 创建 JFrame 窗口骨架

---
+++ src/ScientificCalculator.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class ScientificCalculator extends JFrame{
+    public ScientificCalculator(){
+        setTitle("小花の科学计算器");
+        setSize(500,450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args){
+        ScientificCalculator calc = new ScientificCalculator();
+        calc.setVisible(true);
+        
+
+
+    }
+
+
+}
